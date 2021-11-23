b9ebdb564940b5a93ac1df6ab4775cf3623fe06d
mathew180 <mathewasuku400@gmail.com>
2021-11-23 18:26:43 +0100

    weather-app

---
+++ src/components/Hero/HeroWeather.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroWeather = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HeroWeather
---
+++ src/components/Hero/HeroWeatherElement.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeroWeatherElement = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
