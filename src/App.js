37e246acd5e1d22d504daa889f93cf8411941282
mathew180 <mathewasuku400@gmail.com>
2021-11-29 10:24:07 +0100

    created weather-app-frontend with react js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar/NavbarWeather';
+import { FaBars, FaCloud } from 'react-icons/fa';
 
 function App() {
+  let d = new Date()
+  let days = ["Sun", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
   return (
     <div className="App">
-      <Navbar />
-      
+      <div className="App-container">
+          <form>
+            <input placeholder="Search State"></input>
+            <FaBars className="Fabars"/>
+          </form>
+
+          <nav className="navbar">
+              <ul>   
+                <li className="navbarState">Canada</li>
+                <li>{days[d.getDay()]}, {months[d.getMonth()]} {d.getDate()}th</li>
+              </ul>
+          </nav>
+
+          <banner className="banner">
+             <div>
+             <p>4<span><small><sup>o</sup>C</small></span><br/> </p>
+              <h4><FaCloud className="FaCloud"/>LIGHT RAIN</h4><br/>
+             </div>
+           <div>
+            <ul>
+               <li>2.94m/s <br/> wind speed</li>
+               <li>50% <br/> humidity</li>
+               <li>1016hpa <br/> pressure</li>
+             </ul>
+            </div>
+          </banner>
+
+          <footer className="footer">
+          
+          </footer>
+      </div>
     </div>
   );
 }
