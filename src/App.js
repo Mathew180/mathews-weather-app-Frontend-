2288cfaaec218850d7f05e63fae394272cbc6906
mathew180 <mathewasuku400@gmail.com>
2022-01-03 13:12:14 +0100

    searchLogic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,62 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import { FaBars, FaCloud } from 'react-icons/fa';
 
+const api = {
+  key: "29bb7e14d66b7dca69869864f039d44d",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+  const [queryS, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [state, setState] = useState('tamilnadu');
+
+ const search = evt => {
+   if (evt.key === "Enter"){
+     fetch(`${api.base}weather?q=${state}&units=metric&APPID=${api.key}`)
+     .then(res => res.json())
+     .then(result => {
+       setWeather(result)
+       setQuery('')
+
+       console.log(result)
+     })
+   }
+ }
+
+
+
   let d = new Date()
-  let days = ["Sun", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let days = ["Sun", "Monday", "Tuesday", "Wednesday", "Thurs", "Friday", "Saturday"]
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dec"]
 
   return (
     <div className="App">
       <div className="App-container">
           <form>
-            <input placeholder="Search State"></input>
-            <FaBars className="Fabars"/>
+            <input placeholder="Search State"  
+             value={queryS}
+             onChange={e => setQuery(e.target.value)}
+             onKeyPress={search} />
+         
           </form>
 
-          <nav className="navbar">
+        {(typeof weather.main !== "undefined") ? (
+           <div>
+                      <nav className="navbar">
               <ul>   
-                <li className="navbarState">Canada</li>
-                <li>{days[d.getDay()]}, {months[d.getMonth()]} {d.getDate()}th</li>
+                <li>{weather.name}, {weather.sys.country}</li>
+                <li className="navbarState">{days[d.getDay()]}, {months[d.getMonth()]} {d.getDate()}th</li>
               </ul>
           </nav>
 
-          <banner className="banner">
+          <div className="banner">
              <div>
-             <p>4<span><small><sup>o</sup>C</small></span><br/> </p>
+             <p>{Math.round(weather.main.temp)}<span><small><sup>o</sup>C</small></span> </p>
               <h4><FaCloud className="FaCloud"/>LIGHT RAIN</h4><br/>
              </div>
+
            <div>
             <ul>
                <li>2.94m/s <br/> wind speed</li>
@@ -34,12 +64,16 @@ function App() {
                <li>1016hpa <br/> pressure</li>
              </ul>
             </div>
-          </banner>
+          </div>
 
           <footer className="footer">
           
           </footer>
+          </div> 
+   
+        ) : ('')}
       </div>
+      
     </div>
   );
 }
