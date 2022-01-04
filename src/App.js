dacd96e201e6af0282b500bddb08ab7ea0dfa3a7
mathew180 <mathewasuku400@gmail.com>
2022-01-04 04:22:26 +0100

    implemented-search-function-to-the app

--- src/App.js
+++ src/App.js
@@ -3,78 +3,80 @@ import './App.css';
 import { FaBars, FaCloud } from 'react-icons/fa';
 
 const api = {
-  key: "29bb7e14d66b7dca69869864f039d44d",
+  key: "f975d78f07ca044376b9a0fcb330ef89",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
-  const [queryS, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
-  const [state, setState] = useState('tamilnadu');
 
- const search = evt => {
-   if (evt.key === "Enter"){
-     fetch(`${api.base}weather?q=${state}&units=metric&APPID=${api.key}`)
-     .then(res => res.json())
-     .then(result => {
-       setWeather(result)
-       setQuery('')
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
 
-       console.log(result)
-     })
-   }
- }
 
 
 
   let d = new Date()
-  let days = ["Sun", "Monday", "Tuesday", "Wednesday", "Thurs", "Friday", "Saturday"]
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dec"]
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+  let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
 
   return (
-    <div className="App">
-      <div className="App-container">
-          <form>
-            <input placeholder="Search State"  
-             value={queryS}
-             onChange={e => setQuery(e.target.value)}
-             onKeyPress={search} />
-         
-          </form>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
 
+    <main className="App">
+      <div className="App-container">
+         
+            <input  
+              type="text"
+              className="search-bar"
+              placeholder="Search..."
+              onChange={e => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search} />
+         
+        
+          
         {(typeof weather.main !== "undefined") ? (
            <div>
-                      <nav className="navbar">
-              <ul>   
-                <li>{weather.name}, {weather.sys.country}</li>
-                <li className="navbarState">{days[d.getDay()]}, {months[d.getMonth()]} {d.getDate()}th</li>
-              </ul>
-          </nav>
+              <nav className="navbar">
+                 <ul>   
+                    <li>{weather.name}, {weather.sys.country}</li>
+                    <li className="navbarState">{days[d.getDay()]}, {months[d.getMonth()]} {d.getDate()}</li>
+                  </ul>
+              </nav>
 
-          <div className="banner">
-             <div>
-             <p>{Math.round(weather.main.temp)}<span><small><sup>o</sup>C</small></span> </p>
-              <h4><FaCloud className="FaCloud"/>LIGHT RAIN</h4><br/>
-             </div>
+           <div className="banner">
+              <div>
+                 <p>{Math.round(weather.main.temp)}<span><small><sup>o</sup>C</small></span> </p>
+                 <h4><FaCloud className="FaCloud"/>{weather.weather[0].main}</h4><br/>
+              </div>
 
            <div>
             <ul>
-               <li>2.94m/s <br/> wind speed</li>
-               <li>50% <br/> humidity</li>
-               <li>1016hpa <br/> pressure</li>
+               <li>{Math.round(weather.wind.speed)}m/s <br/> wind speed</li>
+               <li>{Math.round(weather.main.humidity)}% <br/> humidity</li>
+               <li>{Math.round(weather.main.pressure)}hpa <br/> pressure</li>
              </ul>
             </div>
           </div>
 
-          <footer className="footer">
-          
-          </footer>
           </div> 
    
         ) : ('')}
       </div>
-      
+      </main>
     </div>
+
   );
 }
 
